14588993341ee9a7e424b9e4ca84bc16539944e7
hkldtrinanda <hkldtrinanda12@gmail.com>
2021-09-30 01:23:29 +0700

    403-Chapter7-haikaldanungtrinanda

---
+++ SeruvivalShooter/SurvivalShooter/Assets/atkenemy.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class atkenemy : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+ 
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+ 
+ 
+    void Awake ()
+    {
+        //Mencari game object dengan tag "Player"
+        player = GameObject.FindGameObjectWithTag ("Player");
+ 
+        //mendapatkan komponen player health
+        playerHealth = player.GetComponent <PlayerHealth> ();
+ 
+        //mendapatkan komponen Animator
+        anim = GetComponent <Animator> ();
+        //enemyHealth = GetComponent<EnemyHealth>();      
+    }
+ 
+ 
+
+    
+    void OnTriggerEnter (Collider other)
+    {
+        //Set player in range
+        if(other.gameObject == player && other.isTrigger == false)
+        {
+            playerInRange = true;
+        }
+    }
+ 
+    //Callback jika ada object yang keluar dari trigger
+    void OnTriggerExit (Collider other)
+    {
+        //Set player not in range
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+ 
+ 
+    void Update ()
+    {
+        timer += Time.deltaTime;
+ 
+        if(timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
+        {
+            Attack ();
+        }
+ 
+        //mentrigger animasi PlayerDead jika darah player kurang dari sama dengan 0
+        if(playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger ("PlayerDead");
+        }
+    }
+ 
+ 
+    void Attack ()
+    {
+        //Reset timer
+        timer = 0f;
+ 
+        //Taking Damage
+        if(playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage (attackDamage);
+        }
+    }
+
+}
